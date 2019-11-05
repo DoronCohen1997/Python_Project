7ea5fccb23491d33f789c25f8bec1adf74ff5114
Doron Cohen <tami440000@walla.co.il>
2019-11-05 09:52:53 +0200

    Python Project

---
+++ mainWindow.py
@@ -0,0 +1,181 @@
+import sys
+import random
+import os
+import subprocess
+import time
+import threading
+import shlex
+
+from PyQt5.QtCore import QDate
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
+from subprocess import Popen
+from subprocess import call
+from asyncio.windows_utils import PIPE
+from Naked.toolshed.shell import execute_js, muterun_js
+
+date = QDate.currentDate().toString("yyyy.MM.dd")
+newdate = date.replace('.', '-')
+print (date)
+print(newdate)
+
+
+
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTreeWidgetItemIterator
+import ui_mainWindow1
+# from ui_mainWindow import Ui_MainWindow
+
+
+
+class MainWindow(QtWidgets.QMainWindow, ui_mainWindow1.Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        # self.ui = Ui_MainWindow()
+        self.setupUi(self)
+        self.pushButton.clicked.connect(self.click_me)
+        self.btn_test.clicked.connect(self.open_test_files)
+        self.btn_run.clicked.connect(self.run_selected_tests)
+        self.btn_logs.clicked.connect(self.print_all_logs)
+        self.slc_all_btn.clicked.connect(self.select_all_checkboxs)
+        self.rmv_all_btn.clicked.connect(self.remove_all_checkboxs)
+        self.btn_stop.clicked.connect(self.stop_all_running_processes)
+        self.list_of_test = []
+        self.checked_item = []
+        self.processes = []
+        self.file_types = {
+            ".js": "node",
+            ".py": "python"
+        }
+        # self.open_test_files()
+        # self.put_test_list()
+        self.dir = ""
+
+    def click_me(self):
+        mylist = ["doron", "yoval", "siraz", "dani", "yaron"]
+
+        self.pushButton.setText(random.choice(mylist))
+
+    def open_test_files(self):
+        self.dir = QtWidgets.QFileDialog.getExistingDirectory(self, "select folder")
+        if self.dir:
+            self.testslist.clear()
+            for choosetest in os.listdir(self.dir):
+                if choosetest.endswith('.js'):
+                    item = QtWidgets.QTreeWidgetItem(self.testslist, [choosetest])
+                    item.setCheckState(0, QtCore.Qt.Unchecked)
+                    self.list_of_test.append(choosetest)
+                    print(self.list_of_test)
+
+
+
+    def run_test_checked(self):
+        #checked_item = []
+        iterator = QTreeWidgetItemIterator(self.testslist)
+        while iterator.value():
+            item=iterator.value()
+            print(item)
+            iterator+=1
+            if item.checkState(0):
+                item.text(0)
+                self.checked_item.append(item.text(0))
+                print(self.checked_item)
+
+        return self.checked_item
+                #self.run_selected_tests()
+
+    def generate_command_for_file_names(self, file_names):
+        command_list = list(map(lambda file_name: f"{self.run_file_with(file_name)} {file_name}", file_names))
+        return " && ".join(command_list)
+
+    def run_file_with(self, file_name):
+        if not self.file_types:
+            return ""
+        for eof in self.file_types:
+            if file_name.endswith(eof):
+                return self.file_types[eof]
+        return ""
+
+
+    def run_selected_tests(self):
+        #try:
+            checked_files = self.run_test_checked()
+            print(checked_files)
+            if not checked_files:
+                return ""
+            else:
+                for test in self.checked_item:
+                    time.sleep(2)
+                    # print("C:/Users/Dafna Cohen/Desktop/CRM Project"+"/"+test)
+                run_command = self.generate_command_for_file_names(checked_files)
+                process = Popen(run_command, shell=True, cwd=self.dir)
+                self.processes.append({"id": process.pid})
+                print(self.processes)
+            if process.poll() is None:
+                        self.btn_run.setEnabled(False)
+                        self.btn_test.setEnabled(False)
+            else:
+                        self.btn_run.setEnabled(True)
+                        self.btn_test.setEnabled(True)
+
+
+        #except:
+            print("Error")
+
+    def clear(self):
+        self.testslist.clear()
+
+    def print_all_logs(self):
+        logger_file_path = self.dir + "/log/"
+        print(logger_file_path)
+        for file in os.listdir(logger_file_path):
+            if (file.endswith(".log") and (file == str(newdate) +  "-results.log")):
+                        logger_file_full_path = logger_file_path + file
+                        print(logger_file_full_path)
+                        with open(logger_file_full_path, "r") as logger_file:
+                            self.textEdit.append(logger_file.read())
+
+    def select_all_checkboxs(self):
+        iterator = QTreeWidgetItemIterator(self.testslist)
+        while iterator.value():
+            item = iterator.value()
+            print(item)
+            item.setCheckState(0, QtCore.Qt.Checked)
+            iterator += 1
+            self.slc_all_btn.setEnabled(False)
+            self.rmv_all_btn.setEnabled(True)
+
+    def remove_all_checkboxs(self):
+        iterator = QTreeWidgetItemIterator(self.testslist)
+        while iterator.value():
+            item = iterator.value()
+            print(item)
+            item.setCheckState(0, QtCore.Qt.Unchecked)
+            iterator += 1
+            self.rmv_all_btn.setEnabled(False)
+            self.slc_all_btn.setEnabled(True)
+
+
+    def kill(self, process_pid):
+        #self.thread.stop()
+        subprocess.Popen("taskkill /F /T /PID %i" % process_pid, shell=True)
+        #Popen("TASKKILL /F /PID {pid} /T".format(pid=proc_pid))
+
+    def stop_all_running_processes(self):
+        warning_msg = "Are you sure you want to stop all processes??"
+        resp = QtWidgets.QMessageBox.question(self, "Note:", warning_msg, QtWidgets.QMessageBox.Yes,
+                                              QtWidgets.QMessageBox.No)
+        if resp == QtWidgets.QMessageBox.Yes:
+            for p in self.processes:
+                self.kill(p["id"])
+                self.btn_run.setEnabled(True)
+                self.btn_test.setEnabled(True)
+
+
+
+
+
+app = QtWidgets.QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
---
+++ ui_mainWindow1.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'ui_mainWindow1.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1104, 848)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(930, 720, 141, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.btn_test = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_test.setGeometry(QtCore.QRect(110, 80, 161, 41))
+        self.btn_test.setObjectName("btn_test")
+        self.btn_run = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_run.setGeometry(QtCore.QRect(300, 80, 171, 41))
+        self.btn_run.setObjectName("btn_run")
+        self.btn_stop = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_stop.setGeometry(QtCore.QRect(500, 80, 161, 41))
+        self.btn_stop.setObjectName("btn_stop")
+        self.btn_logs = QtWidgets.QPushButton(self.centralwidget)
+        self.btn_logs.setGeometry(QtCore.QRect(710, 80, 161, 41))
+        self.btn_logs.setObjectName("btn_logs")
+        self.testslist = QtWidgets.QTreeWidget(self.centralwidget)
+        self.testslist.setGeometry(QtCore.QRect(110, 140, 256, 561))
+        self.testslist.setObjectName("testslist")
+        self.testslist.headerItem().setText(0, "1")
+        self.rmv_all_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.rmv_all_btn.setGeometry(QtCore.QRect(250, 730, 93, 71))
+        self.rmv_all_btn.setObjectName("rmv_all_btn")
+        self.slc_all_btn = QtWidgets.QPushButton(self.centralwidget)
+        self.slc_all_btn.setGeometry(QtCore.QRect(110, 730, 93, 71))
+        self.slc_all_btn.setObjectName("slc_all_btn")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(493, 146, 381, 551))
+        self.textEdit.setObjectName("textEdit")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1104, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "\"Click me\""))
+        self.btn_test.setText(_translate("MainWindow", "Tests"))
+        self.btn_run.setText(_translate("MainWindow", "Run"))
+        self.btn_stop.setText(_translate("MainWindow", "Stop"))
+        self.btn_logs.setText(_translate("MainWindow", "Logs"))
+        self.rmv_all_btn.setText(_translate("MainWindow", "Remove All"))
+        self.slc_all_btn.setText(_translate("MainWindow", "Select All"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
